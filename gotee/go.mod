f4b0c201630c070d1acc6b49d81c02010472aa63
Fabijan Dokic <f.dokic@gmail.com>
2022-04-13 15:13:06 +0200

    add go source

---
+++ gotee/go.mod
@@ -0,0 +1,3 @@
+module example.com/gotee
+
+go 1.18
---
+++ gotee/gotee.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var teether_path = "/home/fdokic/dev/teether/"
+var contract_table_path = "/home/fdokic/Downloads/con0.csv"
+var result_file_dir = "/home/fdokic/dev/teether/results/"
+
+func main() {
+	fmt.Println("process start")
+	var numCPU = runtime.NumCPU()
+	fmt.Printf("cpus: %v \n", numCPU)
+	analyzeContracts()
+
+}
+
+func checkContract(i int, c chan int, str []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	code := str[1]
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
+	defer cancel()
+	if err := exec.CommandContext(ctx, "python", teether_path+"bin/gen_exploit.py", code, "0x1234", "0x1000", "+1000", result_file_dir+fmt.Sprint(i)+".txt", str[0]).Run(); err != nil {
+		fmt.Println("timeout: " + fmt.Sprint(i))
+		<-c
+		return
+	}
+	/*
+		cmd := exec.Command("python", teether_path+"bin/gen_exploit.py", code, "0x1234", "0x1000", "+1000", result_file_dir+fmt.Sprint(i)+".txt")
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("cmd err: " + fmt.Sprint(i) + " contract " + err.Error())
+		}
+		//fmt.Println(string(out))
+		outStr := string(out)
+		if strings.Contains(outStr, "eth.sendTransaction") {
+			s := str[0] + "\n\n" + str[2] + "\n\n" + str[1] + "\n\n" + outStr
+			err := os.WriteFile(result_file_dir+fmt.Sprint(i)+".txt", []byte(s), 0644)
+			if err != nil {
+				panic(err)
+			}
+		}*/
+	fmt.Printf("checked contract %v \n", i)
+	<-c
+}
+
+func analyzeContracts() {
+	f, err := os.Open(contract_table_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+
+	// drop header
+	_, err = csvReader.Read()
+	if err == io.EOF {
+		log.Fatal(err)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan int, runtime.NumCPU()-2)
+	var wg sync.WaitGroup
+
+	for i := 0; ; i++ {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// do something with read line
+		c <- i
+		wg.Add(1)
+		go checkContract(i, c, rec, &wg)
+
+	}
+
+	wg.Wait()
+}
