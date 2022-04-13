ff3a58c98f10487e8d8a37b41f1829a26dc6b8ca
Fabijan Dokic <f.dokic@gmail.com>
2022-04-13 18:22:35 +0200

    reset git

--- gotee/gotee.go
+++ gotee/gotee.go
@@ -14,24 +14,25 @@ import (
 )
 
 var teether_path = "/home/fdokic/dev/teether/"
-var contract_table_path = "/home/fdokic/Downloads/con0.csv"
-var result_file_dir = "/home/fdokic/dev/teether/results/"
+var contract_table_path = []string{"conv0.csv"}
+var result_file_dir = "results/"
 
 func main() {
 	fmt.Println("process start")
 	var numCPU = runtime.NumCPU()
 	fmt.Printf("cpus: %v \n", numCPU)
-	analyzeContracts()
-
+	for _, p := range contract_table_path {
+		analyzeContracts(teether_path + p)
+	}
 }
 
-func checkContract(i int, c chan int, str []string, wg *sync.WaitGroup) {
+func checkContract(p string, i int, c chan int, str []string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	code := str[1]
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Minute)
 	defer cancel()
-	if err := exec.CommandContext(ctx, "python", teether_path+"bin/gen_exploit.py", code, "0x1234", "0x1000", "+1000", result_file_dir+fmt.Sprint(i)+".txt", str[0]).Run(); err != nil {
-		fmt.Println("timeout: " + fmt.Sprint(i))
+	if err := exec.CommandContext(ctx, "python", teether_path+"bin/gen_exploit.py", code, "0x1234", "0x1000", "+1000", teether_path+result_file_dir+p+fmt.Sprint(i)+".txt", str[0]).Run(); err != nil {
+		fmt.Println("timeout: " + p + fmt.Sprint(i))
 		<-c
 		return
 	}
@@ -50,11 +51,11 @@ func checkContract(i int, c chan int, str []string, wg *sync.WaitGroup) {
 				panic(err)
 			}
 		}*/
-	fmt.Printf("checked contract %v \n", i)
+	fmt.Printf("checked %v %v \n", p, i)
 	<-c
 }
 
-func analyzeContracts() {
+func analyzeContracts(contract_table_path string) {
 	f, err := os.Open(contract_table_path)
 	if err != nil {
 		log.Fatal(err)
@@ -63,15 +64,6 @@ func analyzeContracts() {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
-
-	// drop header
-	_, err = csvReader.Read()
-	if err == io.EOF {
-		log.Fatal(err)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	c := make(chan int, runtime.NumCPU()-2)
 	var wg sync.WaitGroup
@@ -87,7 +79,7 @@ func analyzeContracts() {
 		// do something with read line
 		c <- i
 		wg.Add(1)
-		go checkContract(i, c, rec, &wg)
+		go checkContract(contract_table_path+"_", i, c, rec, &wg)
 
 	}
 
